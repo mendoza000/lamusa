abf62ca456aaf4bf135b5429662d7d4583c51fa3
mendoza000 <omarmendoza.alv25@gmail.com>
2021-10-12 00:04:31 -0400

    añadida la vista de producto abierto

---
+++ src/components/OpenProduct.jsx
@@ -0,0 +1,73 @@
+import React, {useEffect} from 'react'
+import PropTypes from 'prop-types'
+import 'animate.css'
+import './OpenProduct.css'
+
+const OpenProduct = ({img, title, price, category, setViewProduct}) => {
+	const body = document.querySelector('body');
+
+	useEffect(() => {
+		body.style.overflowY = 'hidden'
+	}, [])
+
+	const returnPage = () => {
+		const op = document.querySelector('.openProduct');
+		op.classList.remove('animate__slideInUp')
+		op.classList.add('animate__slideOutDown')
+		body.style.overflowY = ''
+		setTimeout(() =>{
+			setViewProduct(false)
+		}, 550)
+	}
+	
+	return(
+		<div className="openProduct animate__animated animate__slideInUp animate__faster">
+			<button 
+				className="openProduct_btnReturn"
+				onClick={returnPage}>
+				<i className="las la-arrow-left"></i>
+			</button>
+			<h3 className="openProduct_title">
+				La Musa Regalona
+			</h3>
+
+			<div className="openProduct_card">
+				<span className="openProduct_card_category">
+					{category}
+				</span>
+				<span className="openProduct_card_title">
+					{title}
+				</span>
+				<div className="openProduct_circle"></div>
+				<img 
+					src={img} 
+					alt={title}
+					className="openProduct_card_img"/>
+
+				<div className="openProduct_card_containerBtn">
+					<span>
+						<i className="las la-shopping-cart"></i>
+						Carrito (0)
+					</span>
+					<button>
+						<i class="las la-cart-plus"></i>
+						Añadir
+					</button>
+					<span>
+						{price}
+					</span>
+				</div>
+			</div>
+		</div>
+	)
+
+}
+
+export default OpenProduct
+
+OpenProduct.propTypes = {
+	img: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	price: PropTypes.string.isRequired,
+	category: PropTypes.string.isRequired
+}
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,26 +1,50 @@
-import React from 'react'
-import './Product.css'
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+import OpenProduct from './OpenProduct'
+import './Product.css' 
 
 const Product = ({img, title, category, price}) => {
-	
+	const [viewProduct, setViewProduct] = useState(false)
+
 	return(
-		<div className="product">
-			<div className="circle"></div>
-			<img src={img} alt={title}/>
-			<p className="product_title">
-				{title}
-			</p>
-			<div>
-				<span className="product_category">
-					{category}
-				</span>
-				<span className="product_price">
-					{price}
-				</span>
+	    <>
+			<div
+				className="product"
+				onClick={ () => setViewProduct(true) }>
+				<div className="circle"></div>
+				<img src={img} alt={title}/>
+				<p className="product_title">
+					{title}
+				</p>
+				<div>
+					<span className="product_category">
+						{category}
+					</span>
+					<span className="product_price">
+						{price}
+					</span>
+				</div>
 			</div>
-		</div>
-	)
+			
+			{
+				viewProduct 
+				&& <OpenProduct 
+					title={title}
+					img={img}
+					price={price}
+					category={category}
+					setViewProduct={setViewProduct}/>
+			} 
+		</>
+	);
 
+};
+
+export default Product;
+
+Product.propTypes = {
+	img: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	category: PropTypes.string.isRequired,
+	price: PropTypes.string.isRequired
 }
-
-export default Product
